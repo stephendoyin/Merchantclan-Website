bcb4cfdbc3878eab62374dce5dc4a01fe36d5921
stephen <stevedoyin@gmail.com>
2020-08-24 16:51:59 +0100

    initial commit

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,10 @@
+// import "waypoints/lib/noframework.waypoints";
+import NavAnim from "./modules/NavAnimation";
+import MobileNav from "./modules/Mobile-Nav";
+import NavLink from "./modules/Navlink";
+import Slider from "./modules/Slider";
+
+let navAnim = new NavAnim();
+let mobileNav = new MobileNav();
+let navLink = new NavLink();
+let slider = new Slider();
---
+++ gulp/tasks/build.js
@@ -0,0 +1,60 @@
+var gulp = require("gulp"),
+  del = require("del"),
+  usemin = require("gulp-usemin"),
+  rev = require("gulp-rev"),
+  uglify = require("gulp-uglify"),
+  browserSync = require("browser-sync").create(),
+  cleanCSS = require("gulp-clean-css"),
+  { series } = require("gulp");
+
+gulp.task("previewDocs", function () {
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "docs",
+    },
+  });
+});
+
+gulp.task("deleteDistFolder", function () {
+  return del("./docs");
+});
+
+gulp.task("copyGeneralFiles", function () {
+  var pathsToCopy = [
+    "./app/**/*",
+    "!./app/index.html",
+    "!./app/assets/styles/**/*",
+    "!./app/assets/scripts/**/*",
+    "!./app/temp",
+    "!./app/temp/**",
+  ];
+
+  return gulp.src(pathsToCopy).pipe(gulp.dest("./docs"));
+});
+
+gulp.task("usemin", function () {
+  return gulp
+    .src(["./app/index.html"])
+    .pipe(
+      usemin({
+        css: [
+          function () {
+            return rev();
+          },
+          function () {
+            return cleanCSS();
+          },
+        ],
+        js: [
+          function () {
+            return rev();
+          },
+          function () {
+            return uglify();
+          },
+        ],
+      })
+    )
+    .pipe(gulp.dest("./docs"));
+});
