fb411329b94ee066643ccf78f08bfd1ab61f06a3
stephendoyin <stevedoyin@gmail.com>
2020-10-18 19:09:13 +0100

    updated

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,8 +3,13 @@ import NavAnim from "./modules/NavAnimation";
 import MobileNav from "./modules/Mobile-Nav";
 import NavLink from "./modules/Navlink";
 import Slider from "./modules/Slider";
+import CountNumberUpAnim from "./modules/Countup";
+import ScrollRevealScript from "./modules/ScrollReveal";
+
 
 let navAnim = new NavAnim();
 let mobileNav = new MobileNav();
 let navLink = new NavLink();
 let slider = new Slider();
+let countNumberUpAnim = new CountNumberUpAnim();
+let scrollRevealScript = new ScrollRevealScript();
---
+++ app/assets/scripts/modules/Countup.js
@@ -0,0 +1,60 @@
+import { CountUp } from 'countup.js/dist/countUp';
+import "waypoints/lib/noframework.waypoints";
+
+
+
+
+class CountNumberUpAnim{
+    constructor(){
+        this.triggerWaypoint();
+    }
+
+
+    triggerWaypoint(){
+      var waypoint = new Waypoint({
+        element: document.getElementById('slider'),
+        handler: function(direction) {
+          if(direction === "down"){
+            const optionsTwo = {
+                duration: 4,
+                suffix: 'K+',
+              }
+            const options = {
+                duration: 4,
+                suffix: '+',
+              }
+            const countUp = new CountUp('count-up', 500, options);
+            const countUpTwo = new CountUp('count-up-two', 1, optionsTwo);
+            const countUpThree = new CountUp('count-up-three', 500, options);
+            const countUpFour = new CountUp('count-up-four', 15, options);
+            if (!countUp.error) {
+              countUp.start();
+            } else {
+              console.error(countUp.error);
+            }
+            if (!countUpTwo.error) {
+              countUpTwo.start();
+            } else {
+              console.error(countUpTwo.error);
+            }
+            if (!countUpThree.error) {
+              countUpThree.start();
+            } else {
+              console.error(countUpThree.error);
+            }
+            if (!countUpFour.error) {
+              countUpFour.start();
+            } else {
+              console.error(countUpFour.error);
+            }
+          }
+        },
+        offset: '75%'
+      })
+    }
+
+    
+
+}
+
+export default CountNumberUpAnim;
